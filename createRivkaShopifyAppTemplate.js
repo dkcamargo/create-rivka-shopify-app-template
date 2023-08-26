9e63ee7744f7daad50898c548545e0de72a29392
Douglas Camargo <douglas@thecodesoup.com>
2023-08-25 23:10:04 -0300

    chore, fix(template):

---
+++ createRivkaShopifyAppTemplate.js
@@ -0,0 +1,102 @@
+const spawn = require('cross-spawn');
+const path = require('path');
+
+function isUsingYarn() {
+    return (process.env.npm_config_user_agent || '').indexOf('yarn') === 0;
+}
+function init() {
+    const projectName = process.argv[2];
+
+    const root = path.resolve(projectName);
+
+    console.log(`
+        Begining to Create Shopify App Template for  ${projectName} on: ${root}
+        
+        Go get a coffee it might take a few minutes!
+    `);
+
+
+    spawn.sync('git', [`clone`, 'https://github.com/dkcamargo/rivka-shopify-app-template.git', root], { stdio: 'inherit' });
+    spawn.sync('dir', { stdio: 'inherit' });
+    spawn.sync('cd', [root], { stdio: 'inherit' });
+
+    //create node_modules before npm install?
+    let everythingAlright;
+    try {
+        if(isUsingYarn()) {
+            spawn.sync('yarn', ['--cwd', root.toString()], { stdio: 'inherit' });
+        } else {
+            spawn.sync('npm', [`install`, '--prefix', root.toString()], { stdio: 'inherit' });
+        }
+
+        everythingAlright = true;
+    } catch(e) {
+        console.error('Some error happened when trying to install the node_modules')
+        everythingAlright = false;
+    }
+    if(everythingAlright) {
+        console.log(`
+            ${projectName}
+            │   .gitignore
+            │   LICENSE.md
+            │   package.json
+            │   README.md
+            │
+            └───web
+                │   gdpr.js
+                │   index.js
+                │   package.json
+                │   shopify.js
+                │   shopify.web.toml
+                │
+                └───frontend
+                    │   App.jsx
+                    │   dev_embed.js
+                    │   .gitignore
+                    │   index.html
+                    │   index.jsx
+                    │   LICENSE.md
+                    │   package.json
+                    │   Routes.jsx
+                    │   shopify.web.toml
+                    │   vite.config.js
+                    │
+                    ├───assets
+                    │       empty-state.svg
+                    │       home-trophy.png
+                    │       index.js
+                    │
+                    ├───hooks
+                    │       index.js
+                    │       useAppQuery.js
+                    │       useAuthenticatedFetch.js
+                    │
+                    ├───pages
+                    │       ExamplePage1.jsx
+                    │       ExamplePage2.jsx
+                    │       ExitIframe.jsx
+                    │       HomePage.jsx
+                    │       index.jsx
+                    │       NotFound.jsx
+                    │
+                    └───providers
+                            AppBridgeProvider.jsx
+                            index.js
+                            PolarisProvider.jsx
+                            QueryProvider.jsx
+
+            Done Creating ${projectName} Shopify App Template
+            
+            cd ./${projectName}
+            npm run dev
+
+            Happy Hacking!
+        `);
+    }
+
+}
+
+
+module.exports = {
+    init
+};
